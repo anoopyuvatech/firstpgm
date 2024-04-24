998654261d20325929d7c18c731aa99e4126af4e
Anoop <anoop.yuvatech@gmail.com>
2024-04-24 09:31:33 +0530

    first commit

---
+++ hello_app/views.py
@@ -0,0 +1,34 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+def print_hello(request):
+    movie_data= {'movies': [
+        {
+       'title':'Godfather',
+       'year':1990,
+       'summary':'Story of an Family',
+       'sucess':True
+        },
+        {
+       'title':'LEO',
+       'year':2023,
+       'summary':'Story of an Man',
+       'sucess':True
+        },
+        {
+       'title':'Kannur Squad',
+       'year':2023,
+       'summary':'Story of an Police officer',
+       'sucess':True
+        },
+        {
+       'title':'Titanic',
+       'year':1990,
+       'summary':'Story of an Ship',
+       'sucess':True
+        }
+    ]
+    }
+    
+    return render(request,'hello2.html',movie_data)
