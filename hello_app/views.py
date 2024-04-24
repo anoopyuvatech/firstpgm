50b54586e83c74f6b94a004a131886f0e1bacd2b
Anoop <anoop.yuvatech@gmail.com>
2024-04-24 09:36:47 +0530

    second commit

--- hello_app/views.py
+++ hello_app/views.py
@@ -32,3 +32,34 @@ def print_hello(request):
     }
     
     return render(request,'hello2.html',movie_data)
+
+def print_hello1(request):
+    movie_data= {'movies': [
+        {
+       'title':'Godfather',
+       'year':1990,
+       'summary':'Story of an Family',
+       'sucess':True
+        },
+        {
+       'title':'LEO',
+       'year':2023,
+       'summary':'Story of an Man',
+       'sucess':True
+        },
+        {
+       'title':'Kannur Squad',
+       'year':2023,
+       'summary':'Story of an Police officer',
+       'sucess':True
+        },
+        {
+       'title':'Titanic',
+       'year':1990,
+       'summary':'Story of an Ship',
+       'sucess':True
+        }
+    ]
+    }
+    
+    return render(request,'hello2.html',movie_data)
